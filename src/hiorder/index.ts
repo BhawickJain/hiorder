c6f2a7a5749490654f66a54b4f4580aa1bbe318f
BhawickJain <bhawick@outlook.com>
2022-11-02 21:11:29 +0000

    lint and format

--- src/hiorder/index.ts
+++ src/hiorder/index.ts
@@ -5,4 +5,4 @@ export { find } from "./find";
 export { map } from "./map";
 export { reduce } from "./reduce";
 export { repeat } from "./repeat";
-export { partition} from "./partition";
+export { partition } from "./partition";
