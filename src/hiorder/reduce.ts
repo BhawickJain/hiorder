9c7cf9af3f01cc02a66ad999ba820e6e4b9dd231
BhawickJain <bhawick@outlook.com>
2023-03-13 18:03:24 +0000

    add docstring

--- src/hiorder/reduce.ts
+++ src/hiorder/reduce.ts
@@ -1,3 +1,28 @@
+/**
+ * Given an array of type T, return an array of T or U when reduced with a function that take two argument and returns one of either type T or a new type U
+ *
+ * @remarks
+ * read about reduce (also known as Fold) on [wikipedia](https://en.wikipedia.org/wiki/Fold_%28higher-order_function%29)
+ *
+ * @example
+ * Sum an array of Numbers with reduce (type T -> T example)
+ * ```ts
+ * reduce([1, 2, 3], (a: number, b: number) => a+b, 0)
+ * // 6
+ * ```
+ *
+ * @example
+ * Return a contenated string of numbers from array of numbers (type T -> U example)
+ * ```ts
+ * reduce([1, 2, 3], (a: string, b: number) => `${a}${b}`, "")
+ * // "123"
+ * ```
+ *
+ * @param arr - array of type T to be reduced
+ * @param callbackFn  - function that will perform the reduction (previous_result, current_value) => new_result
+ * @param initialValue - initial value of previous_result, if another type U is returned, previous_result must also be type U.
+ * @returns single value of T or U, respective of the return type of the function.
+ */
 export function reduce<T, U>(
   arr: T[],
   callbackFn: (prev: U, curr: T, currentIndex?: number, arr?: T[]) => U,
