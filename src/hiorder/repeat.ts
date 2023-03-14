97e8ebd4234cc4bd7ea1af94af912162875a3738
BhawickJain <bhawick@outlook.com>
2023-03-14 18:48:32 +0000

    doc strings with example

--- src/hiorder/repeat.ts
+++ src/hiorder/repeat.ts
@@ -1,3 +1,15 @@
+/**
+ * Given a callback function, repeated call the function for a specified number. Returned result from the function is not captured and returned.
+ * @example
+ * ```ts
+ * repeat(3, () => console.log('hello world'))
+ * // => hello world
+ * // => hello world
+ * // => hello world
+ * ```
+ * @param repeat - the number of times callbackFn is called
+ * @param callbackFn - the callback function to be run repeatedly
+ */
 export function repeat<T>(repeat: number, callbackFn: () => T): void {
   for (let i = 0; i < repeat; i++) {
     callbackFn();
