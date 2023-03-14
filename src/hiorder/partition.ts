d5810ba8e2b73be34d971bd4cd7f9b744ba9b411
BhawickJain <bhawick@outlook.com>
2023-03-14 18:57:07 +0000

    doc strings and example

--- src/hiorder/partition.ts
+++ src/hiorder/partition.ts
@@ -1,3 +1,14 @@
+/**
+ * Takes an array with a function that can take each array item and evaluate it to a boolean value, returns one array containing an array of true items and false items respectively.
+ * @example
+ * ```ts
+ * partition([1, -1, 2, -2, 3, -3], (it: number) => it < 0)
+ * // => [ [ -1, -2, -3 ], [ 1, 2, 3 ] ]
+ * ```
+ * @param arr - array of type T to be partitioned into two arrays
+ * @param callbackFn - function that will be used to partition the array, must take array item and return boolean.
+ * @returns returns an array of length two containing an array item evaluated as true at index position 0 and array items evaluated as false at index position 1
+ */
 export function partition<T>(
   arr: Array<T>,
   callbackFn: (el: T) => boolean,
